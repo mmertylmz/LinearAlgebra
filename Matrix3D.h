a7822926a291eef6435ce58c128de8f26c7aa83f
Mert Can Yılmaz <dokkensavatage@gmail.com>
2025-02-12 22:50:58 +0300

    Matrix 3D basic struct

---
+++ Matrix3D.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Vector3D.h"
+
+struct Matrix3D 
+{
+private:
+	float n[3][3];
+public:
+	Matrix3D();
+	Matrix3D(float n00, float n01, float n02,
+		float n10, float n11, float n12,
+		float n20, float n21, float n22);
+	Matrix3D(const Vector3D& a, const Vector3D& b, const Vector3D& c);
+
+	float& operator ()(int i, int j);
+	const float& operator ()(int i, int j) const;
+
+	Vector3D& operator [](int j);
+	const Vector3D& operator [](int j) const;
+
+};
+
+void PrintMatrix3D(const Matrix3D& m, const std::string& title);
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include "Vector3D.h"
+#include "Matrix3D.h"
 
 int main()
 {
+    /*
     Vector3D v(1.0f, 2.0f, 3.0f);
     PrintVector3D(v);
 
@@ -9,6 +10,29 @@ int main()
     Vector3D result = v + w;
 
     PrintVector3D(result);
+    */
+    
+	Matrix3D m1(1.0f, 2.0f, 3.0f,
+		4.0f, 5.0f, 6.0f,
+		7.0f, 8.0f, 9.0f);
+
+	PrintMatrix3D(m1, "Matrix m1");
+
+	float value = m1(1, 0);
+	//std::cout << value << std::endl;
+
+	m1(1, 0) = 10.0f;
+	PrintMatrix3D(m1, "Matrix m1");
+
+	/*
+	Vector3D a(1.0f, 2.0f, 3.0f);
+	Vector3D b(4.0f, 5.0f, 6.0f);
+	Vector3D c(7.0f, 8.0f, 9.0f);
+	Matrix3D m2(a, b, c);
+
+	PrintMatrix3D(m2, "Matrix m2");
+	*/
+
 
     return 0;
 }
