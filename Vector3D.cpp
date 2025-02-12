523d7dd4a493276adbd67145939c9b1214a6095d
Mert Can Yılmaz <dokkensavatage@gmail.com>
2025-02-12 22:50:58 +0300

    Vector .h .cpp

---
+++ Vector3D.cpp
@@ -0,0 +1,81 @@
+#include "Vector3D.h"
+
+Vector3D::Vector3D() = default;
+Vector3D::Vector3D(float a, float b, float c) : x(a), y(b), z(c) {}
+
+float& Vector3D::operator[](int i)
+{
+    return ((&x)[i]);
+}
+
+const float& Vector3D::operator[](int i) const
+{
+    return ((&x)[i]);
+}
+
+Vector3D& Vector3D::operator*=(float s)
+{
+    x *= s;
+    y *= s;
+    z *= s;
+    return *this;
+}
+
+Vector3D& Vector3D::operator/=(float s)
+{
+    x /= s;
+    y /= s;
+    z /= s;
+    return *this;
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D& v)
+{
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+
+Vector3D& Vector3D::operator-=(const Vector3D& v)
+{
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+}
+
+Vector3D operator*(const Vector3D& v, float s)
+{
+    return Vector3D(v.x * s, v.y * s, v.z * s);
+}
+
+Vector3D operator/(const Vector3D& v, float s)
+{
+    return Vector3D(v.x / s, v.y / s, v.z / s);
+}
+
+Vector3D operator+(const Vector3D& a, const Vector3D& b)
+{
+    return Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+Vector3D operator-(const Vector3D& a, const Vector3D& b)
+{
+    return Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+float Magnitude(const Vector3D& v)
+{
+    return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+Vector3D Normalize(const Vector3D& v)
+{
+    return v / Magnitude(v);
+}
+
+void PrintVector3D(const Vector3D& v)
+{
+    std::cout << "Vector3D(" << v.x << ", " << v.y << ", " << v.z << ")\n";
+}
