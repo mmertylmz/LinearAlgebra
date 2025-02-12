9ebc017ffa2ee17d7bb6207cb09c84f2353b603f
Mert Can Yılmaz <dokkensavatage@gmail.com>
2025-02-12 22:51:00 +0300

    Defining Transform Matrix Header

---
+++ Transform4D.h
@@ -0,0 +1,25 @@
+#include "Matrix4D.h"
+#include "Point3D.h"
+
+struct Transform4D : Matrix4D 
+{
+	Transform4D();
+	Transform4D(float n00, float n01, float n02, float n03,
+		float n10, float n11, float n12, float n13,
+		float n20, float n21, float n22, float n23,
+		float n30, float n31, float n32, float n33);
+
+	Transform4D(const Vector3D& a, const Vector3D& b, const Vector3D& c, const Point3D& p);
+
+	Vector3D& operator [](int j);
+	const Vector3D& operator [](int j) const;
+
+	const Point3D& GetTranslation(void) const;
+	const Point3D& SetTranslation(const Point3D& p);
+};
+
+Transform4D Inverse(const Transform4D& t);
+
+Transform4D operator *(const Transform4D& A, const Transform4D& B);
+Vector3D operator *(const Transform4D& A, const Vector3D& V);
+Point3D operator *(const Transform4D& A, const Point3D& P);
